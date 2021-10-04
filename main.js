02415d49c5381cfa555cbfc658ce02860b305337
Heitor José Tessaro <heitor.j.tessaro@gmail.com>
2021-10-04 18:21:16 -0300

    add reset color function

--- main.js
+++ main.js
@@ -1,31 +1,38 @@
 // Constants
-const DEFAULT_SIZE = 68
+const DEFAULT_SIZE = 16
 
 const gridContainer = document.getElementById('gridContainer')
 const girdSize = document.getElementById('gridSize');
 
-girdSize.onchange = function() {
+girdSize.onchange = function () {
+    resetColor();
     createGrid(girdSize.value);
 }
-// addEventListener()
-// mainContainer.appendChild(unitDiv);
-
-
 
 function createGrid(size) {
     gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`
     gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`
     for (let index = 0; index < size * size; index++) {
         let unitDiv = document.createElement('div');
+
         unitDiv.addEventListener('mouseover', changeColor)
         gridContainer.appendChild(unitDiv);
     }
 }
 
-function changeColor(e){
+function changeColor(e) {
     e.target.style.backgroundColor = '#333333';
     console.log(e);
 }
+
+function resetColor() {
+    let cells = gridContainer.querySelectorAll('div');
+    for (let i = 0; i < cells.length; i++) {
+        cells[i].style.backgroundColor = '#FFFFFF';
+    }
+
+}
+
 
 window.onload = () => {
     createGrid(DEFAULT_SIZE)
